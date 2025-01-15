f6c2dcd9d94e0ed25a7728c48f2b1129540f54ac
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-01-15 16:12:09 +0100

    Hello from Symfony without WebpackEncoreBundle, ViteBundle or AssetMapper

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -7,11 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class FrontController extends AbstractController{
-    #[Route('/front', name: 'app_front')]
+    #[Route('/', name: 'app_front')]
     public function index(): Response
     {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
-        ]);
+        return $this->render('front/index.html.twig');
     }
 }
